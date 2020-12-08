0291343d51e15eecadf903d6d284a6302eb61c48
justin0979 <justinm0979@gmail.com>
2020-12-07 20:13:59 -0600

    base setup of comments server

--- comments/index.js
+++ comments/index.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get("/", (req, res) => res.send("comments"));
+const commentsByPostId = {};
+
+app.get("/posts/:id/comments", (req, res) => {});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+});
 
 app.listen(4001, () => console.log("Comments up on 4001"));
