9d28c2ec37dc0f291ae5381b3b79a63a1f1a92e2
justin0979 <justinm0979@gmail.com>
2020-12-09 17:51:21 -0600

    add GET/POST to comments

--- comments/index.js
+++ comments/index.js
@@ -7,10 +7,21 @@ app.use(bodyParser.json());
 
 const commentsByPostId = {};
 
-app.get("/posts/:id/comments", (req, res) => {});
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id || []]);
+});
 
 app.post("/posts/:id/comments", (req, res) => {
-  const id = randomBytes(4).toString("hex");
+  const commentId = randomBytes(4).toString("hex");
+  const { content } = req.body;
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
 });
 
 app.listen(4001, () => console.log("Comments up on 4001"));
