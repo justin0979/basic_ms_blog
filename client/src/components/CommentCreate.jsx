72e57c6bdb3390303524d3259dc7d81ff99ebdb3
justin0979 <justinm0979@gmail.com>
2020-12-12 15:50:43 -0600

    add comment posting in client

--- client/src/components/CommentCreate.jsx
+++ client/src/components/CommentCreate.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-export default ({ id }) => {
+export default ({ postId }) => {
+  const [content, setContent] = useState("");
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios.post(
+      `http://localhost:4001/posts/{$postId}/comments`,
+      {
+        content,
+      }
+    );
+
+    // Reset content to let user know that their comment has
+    // been persisted.
+    setContent("");
+  };
+
   return (
     <div>
-      <h1>Post Liost</h1>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="">New Comment</label>
+          <input
+            type="text"
+            className="form-control"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
     </div>
   );
 };
