1967dd4372a202f71be4b04e9be661b4eadbb4e2
justin0979 <justinm0979@gmail.com>
2020-12-14 22:36:03 -0600

    event-bus emits post created & comment created events.

--- events/index.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-
-const app = express();
-
-app.listen(4003, () => console.log("Events up on port 4003"));
--- query/index.js
+++ query/index.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.get("/events", (req, res) => {
+  res.send("ok");
+});
+
 app.listen(4002, () => console.log("Query up on port 4002"));
