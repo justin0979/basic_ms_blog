936a587674c5be7e6b6917bc98ce858b6811f057
justin0979 <justinm0979@gmail.com>
2020-12-19 20:51:21 -0600

    fix content placement in comments event

--- client/src/components/CommentList.jsx
+++ client/src/components/CommentList.jsx
@@ -2,22 +2,45 @@ import React /*, { useState, useEffect } */ from "react";
 // import axios from "axios";
 
 export default ({ comments /* postId */ }) => {
-  //  const [comments, setComments] = useState([]);
+  /*
+    Code below made requests to comments events, making 
+    application have requests to posts and comments.
+    Adding a query event made it where this client does
+    NOT have to make a request to comments also, just 
+    posts.
 
-  //  const fetchData = async () => {
-  //    const res = await axios.get(
-  //      `http://localhost:4001/posts/${postId}/comments`
-  //    );
-  //
-  //    setComments(res.data);
-  //  };
+    const [comments, setComments] = useState([]);
 
-  //  useEffect(() => {
-  //    fetchData();
-  //  }, []);
+    const fetchData = async () => {
+      const res = await axios.get(
+        `http://localhost:4001/posts/${postId}/comments`
+      );
+  
+      setComments(res.data);
+    };
+
+    useEffect(() => {
+      fetchData();
+    }, []);
+
+  */
 
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
+
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
+
+    if (comment.status === "rejected") {
+      content = "This comment had been rejected";
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderedComments}</ul>;
